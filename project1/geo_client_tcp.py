426a418bebdcf85e1e001358acb9b33eba116945
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-09-06 20:59:25 -0500

    project 1 done

--- project1/geo_client_tcp.py
+++ project1/geo_client_tcp.py
@@ -12,7 +12,22 @@ PORT = 4300
 def client():
     '''Main client loop'''
     # TODO: Implement client-side tasks
-    pass
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        print('Connected to {}:{}'.format(HOST, PORT))
+
+        target = input(">Enter a country or BYE to quit: \n")
+
+        while target != "BYE":
+            s.sendall("{}".format(target).encode())
+            data = s.recv(1024)
+
+            print('+{}'.format(data.decode()))
+            target = input(">Enter a country or BYE to quit: \n")
+
+        # Close the socket.
+        s.close()
+        print('Connection closed')
 
 def main():
     '''Main function'''
--- project1/geo_server_tcp.py
+++ project1/geo_server_tcp.py
@@ -3,6 +3,7 @@ GEO TCP Server
 '''
 #!/usr/bin/env python3
 
+import time
 from socket import socket, AF_INET, SOCK_STREAM
 
 FILE_NAME = 'geo_world.txt'
@@ -13,13 +14,39 @@ PORT = 4300
 def read_file(filename: str) -> dict:
     '''Read world territories and their capitals from the provided file'''
     world = dict()
+    print("Reading a file...")
+    start = time.time()
+    with open(filename, "r+") as infile:
+        for line in infile:
+            line = line.split(" - ")
+            world[line[0]] = line[1].replace("\n","")
+    end = time.time()
+    print("Read in {:.4f} sec".format(end-start))
     return world
 
 
 def server(world: dict) -> None:
     '''Main server loop'''
     # TODO: Implement server-side tasks
-    pass
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        print('Listening on localhost:{}'.format(PORT))
+        conn, addr = s.accept()
+        with conn:
+            print('Connected to {}'.format(addr[0]))
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    print('Disconnected: {}'.format(addr[0]))
+                    break
+                country = data.decode()
+                print("User query: {}".format(country))
+                if country not in world:
+                    conn.sendall("{}".format("There is no such country.").encode())
+                else:
+                    capital = world[country]
+                    conn.sendall("{}".format(capital).encode())
 
 
 def main():
