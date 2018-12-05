981c3e9ddf7be10d48a445c7a30048e892b744e8
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-12-05 17:03:54 -0600

    all functionalities work

--- final_project/router_1.py
+++ final_project/router_1.py
@@ -42,32 +42,88 @@ def read_file(filename: str) -> None:
 
 def format_update():
     """Format update message"""
-    raise NotImplementedError
+    msg = bytearray()
+    msg.append(0)
+    for item in ROUTING_TABLE:
+        neigh_parts = [int(elem) for elem in item.split(".")]
+        for p in neigh_parts:
+            msg.append(p)
+        cost = ROUTING_TABLE[item][0]
+        msg.append(cost)
+    
+    return msg
 
 
 def parse_update(msg, neigh_addr):
     """Update routing table"""
-    raise NotImplementedError
+    update_flag = False
+    data = [(".".join([str(j) for j in msg[i:i+4]]), msg[i+4]) for i in range(1, len(msg), 5)]
+
+    for neigh_pair in data:
+        data_addr, cost = neigh_pair[0], neigh_pair[1]
+        if data_addr != THIS_NODE:
+            if data_addr in ROUTING_TABLE:
+                if ROUTING_TABLE[neigh_addr][0] + cost < ROUTING_TABLE[data_addr][0]:
+                    ROUTING_TABLE[data_addr][0] = ROUTING_TABLE[neigh_addr][0] + cost
+                    ROUTING_TABLE[data_addr][1] = neigh_addr
+                    update_flag = True
+            else:
+                ROUTING_TABLE[data_addr] = [ROUTING_TABLE[neigh_addr][0] + cost]
+                ROUTING_TABLE[data_addr].append(neigh_addr)
+                update_flag = True
+    
+    if update_flag == True:
+        return True
+    return False
 
 
 def send_update(node):
     """Send update"""
-    raise NotImplementedError
+    client_sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_sckt.bind((THIS_NODE, 4300))
+    packet = format_update()
+    CLIENT_PORT = 4300 + int(node[-1])
+    client_sckt.sendto(packet, (node, CLIENT_PORT))
+    client_sckt.close()
 
 
 def format_hello(msg_txt, src_node, dst_node):
     """Format hello message"""
-    raise NotImplementedError
+    msg = bytearray()
+    msg.append(1)
+    src_node = src_node.split(".")
+    for part in src_node:
+        msg.append(int(part))
+    dst_node = dst_node.split(".")
+    for part in dst_node:
+        msg.append(int(part))
+    for part in msg_txt:
+        msg.append(ord(part))
+    
+    return msg
 
 
 def parse_hello(msg):
     """Send the message to an appropriate next hop"""
-    raise NotImplementedError
+    src  = ".".join([str(p) for p in msg[1:5]])
+    dest = ".".join([str(p) for p in msg[5:9]])
+    msg  = "".join([chr(p) for p in msg[9:]])
+
+    if dest == THIS_NODE:
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print(f"{current_time} | Received {msg} from {src}")
+    else:
+        send_hello(msg, THIS_NODE, dest)
 
 
 def send_hello(msg_txt, src_node, dst_node):
     """Send a message"""
-    raise NotImplementedError
+    client_sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_sckt.bind((THIS_NODE, 4300))
+    packet = format_hello(msg_txt, src_node, dst_node)
+    CLIENT_PORT = 4300 + int(dst_node[-1])
+    client_sckt.sendto(packet, (dst_node, CLIENT_PORT))
+    client_sckt.close()
 
 
 def print_status():
@@ -89,15 +145,49 @@ def main(args: list):
 
     print(f"{current_time} | Listening on {THIS_NODE}:{PORT}")
     
-
     read_file(args[1])
 
     print_status()
 
-    while True:
-        exit()
-        (data, client_addr) = server_sckt.recvfrom(1024)
+    time.sleep(4)
 
+    for n in NEIGHBORS:
+        send_update(n)
+
+    inputs = [server_sckt]
+    outputs = []
+
+    while len(inputs) > 0:
+        reads, writes, errors = select.select(
+            inputs, outputs, inputs)
+        
+        for s in reads:
+            if s == server_sckt:
+                server_sckt.setblocking(False)
+                data, client_addr = server_sckt.recvfrom(1024)
+            else:
+                data, client_addr = s.recvfrom(1024)
+
+            if data[0] == 0:
+                if parse_update(data, client_addr[0]) == True:
+                    current_time = time.strftime("%H:%M:%S", time.localtime())
+                    print(f"{current_time} | Table updated with information from {client_addr[0]}")
+                    print_status()
+                    for neigh_addr in NEIGHBORS:
+                        send_update(neigh_addr)
+            else:
+                parse_hello(data)
+
+
+        my_choice = random.randint(0,100)
+
+        if my_choice < 10:
+            msg = random.choice(MESSAGES)
+            dest = random.choice(list(ROUTING_TABLE.keys()))
+            current_time = time.strftime("%H:%M:%S", time.localtime())
+            next_hop = ROUTING_TABLE[dest][1]
+            print(f"{current_time} | Sending {msg} to {dest} via {next_hop}")
+            send_hello(msg, THIS_NODE, dest)
 
 if __name__ == "__main__":
     main(sys.argv)
